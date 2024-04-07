08eeb033a8129296649555c92f6976500ba0f932
DmytroLysachenko <157533371+DmytroLysachenko@users.noreply.github.com>
2024-04-07 20:01:17 +0000

    Deploying to gh-pages from @ DmytroLysachenko/goit-js-hw-10@c8698965cb807d995c6991b688c68100ff1b2c87 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";const t=document.querySelector(".form");document.querySelectorAll("input");t.addEventListener("click",e=>{e.target.nodeName==="button"&&e.preventDefault()});console.log(delayInput);
+//# sourceMappingURL=commonHelpers2.js.map
