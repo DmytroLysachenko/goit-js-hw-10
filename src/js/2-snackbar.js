7f281e3686e6a5e2c899617fa496ae9d591ac781
Dimka863 <dlysachenko98@gmail.com>
2024-04-08 11:09:33 +0200

    another link to imgs

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -21,7 +21,7 @@ const makePromise = ({ delay, shouldResolve }) => {
       backgroundColor: 'orange',
       theme: 'dark',
       color: 'orange',
-      iconUrl: '/achtung.png',
+      iconUrl: './achtung.png',
       position: 'topRight',
     });
   }
@@ -45,7 +45,7 @@ form.addEventListener('submit', event => {
         backgroundColor: 'green',
         theme: 'dark',
         color: 'green',
-        iconUrl: '/ok.png',
+        iconUrl: './ok.png',
         position: 'topRight',
       })
     )
@@ -56,7 +56,7 @@ form.addEventListener('submit', event => {
         backgroundColor: 'red',
         theme: 'dark',
         color: 'red',
-        iconUrl: '/ok.png',
+        iconUrl: './ok.png',
         position: 'topRight',
       });
     });
